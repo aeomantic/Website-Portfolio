aeaffac26c4eb0f6457174d6c6ae1ef3b788a256
aeomantic <108773869+aeomantic@users.noreply.github.com>
2025-01-01 12:33:03 +0800

    First Commit
    First Date 01/01/2025

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import '../Styles/modern-normalize.css';
+import '../Styles/style.css';
+import '../Styles/Components/header.css';
+import '../Styles/Components/hero.css';
+import '../Styles/Components/about.css';
+import '../Styles/Components/featured.css';
+import '../Styles/Components/work.css';
+import '../Styles/Components/contact.css';
+import '../Styles/Components/footer.css';
+import '../Styles/Components/mobile-nav.css';
+import '../Styles/utils.css';
+
+import mobileNav from './utilis/mobile-nav';
+
+mobileNav();
+
